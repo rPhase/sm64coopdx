7a5950a59d41f5695415fe7f1e6ce65fdbeb7d75
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-11-24 14:21:51 +0330

    m

--- src/pc/platform.h
+++ src/pc/platform.h
@@ -25,10 +25,16 @@ int sys_strcasecmp(const char *s1, const char *s2);
 #ifdef TARGET_ANDROID
 const char* get_gamedir(void);
 #endif
+
+#ifdef _WIN32
+bool sys_windows_short_path_from_wcs(char *destPath, size_t destSize, const wchar_t *wcsLongPath);
+bool sys_windows_short_path_from_mbs(char* destPath, size_t destSize, const char *mbsLongPath);
+#endif
 const char *sys_user_path(void);
 const char *sys_exe_path(void);
 const char *sys_file_extension(const char *fpath);
 const char *sys_file_name(const char *fpath);
+void sys_swap_backslashes(char* buffer);
 
 // shows an error message in some way and terminates the game
 void sys_fatal(const char *fmt, ...) __attribute__ ((noreturn));
