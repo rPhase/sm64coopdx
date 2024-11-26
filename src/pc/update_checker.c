bcba3cbf96be04afcaaa49cf376fc7c5802822d0
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-11-26 20:06:24 +0330

    stdbool

--- src/pc/update_checker.c
+++ src/pc/update_checker.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 bool gUpdateMessage = false;
 
 #ifndef TARGET_ANDROID
--- src/pc/update_checker.h
+++ src/pc/update_checker.h
@@ -1,8 +1,8 @@
 #ifndef _UPDATE_CHECKER_H
 #define _UPDATE_CHECKER_H
+#include <stdbool.h>
 extern bool gUpdateMessage;
 #ifndef TARGET_ANDROID
-#include <stdbool.h>
 
 void show_update_popup(void);
 void check_for_updates(void);
