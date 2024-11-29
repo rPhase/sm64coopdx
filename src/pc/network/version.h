b6aa021735ef5cdebcd5179c74de42b3b10c120a
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-11-29 17:47:17 +0330

    re-add beta

--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -14,7 +14,7 @@
 #else
     // internal version
     #define MAX_PLATFORM_VERSION 3
-    #define VERSION_TEXT "android "
+    #define VERSION_TEXT "beta android"
 #endif
 
 #define VERSION_NUMBER 37
