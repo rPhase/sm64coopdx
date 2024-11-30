61eec1cc8c60748c4c0662d735cac07506576c59
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-12-01 00:29:30 +0330

    mod_storage.cpp for the 672th time

--- src/pc/mods/mod_storage.cpp
+++ src/pc/mods/mod_storage.cpp
@@ -1,14 +1,21 @@
 #include "mod_storage.h"
 
-#include <stdio.h>
-#include <ctype.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "pc/mini.h"
+
+extern "C" {
 #include "pc/platform.h"
-#include "pc/configini.h" // for writing
-#include "pc/ini.h" // for parsing
+#include "pc/mods/mod.h"
 #include "pc/lua/smlua.h"
 #include "pc/mods/mods_utils.h"
+#include "pc/fs/fs.h"
 #include "pc/debuglog.h"
-#include <stdbool.h>
+}
+
+#define path_exists fs_sys_path_exists
+
 #ifdef __ANDROID__
 #include "pc/utils/misc.h"
 
@@ -20,11 +27,13 @@ typedef struct CachedKey {
 
 static CachedKey sCachedKeys[MAX_CACHED_KEYS];
 
+extern "C" {
 void key_cache_init(void) {
     for (u32 i = 0; i < MAX_CACHED_KEYS; i++) {
         snprintf(sCachedKeys[i].key, MAX_KEY_VALUE_LENGTH, "%s", "");
         snprintf(sCachedKeys[i].value, MAX_KEY_VALUE_LENGTH, "%s", "");
     }
+}
 }
 
 char *key_cached(char key[], char value[]) {
@@ -77,7 +86,7 @@ void strdelete(char string[], char substr[]) {
     string[i] = '\0';
 }
 
-bool char_valid(char* buffer) {
+bool Char_Valid(char* buffer) {
     if (buffer[0] == '\0') { return false; }
     while (*buffer != '\0') {
         if ((*buffer >= 'a' && *buffer <= 'z') || (*buffer >= 'A' && *buffer <= 'Z') || (*buffer >= '0' && *buffer <= '9') || *buffer == '_' || *buffer == '.' || *buffer == '-') {
@@ -89,52 +98,21 @@ bool char_valid(char* buffer) {
     return true;
 }
 
-s32 key_count(char* filename) {
-    FILE *file;
-    file = fopen(filename, "r");
-    if (file == NULL) { return 0; }
-
-#ifdef __ANDROID__
-    // something is wrong with the original
-    // implementation of this function (seen below
-    // in the else block) on Android, so copy the
-    // logic used in mod.c which seems to work
-    // better
-    s32 lines = 0;
-    char buffer[512] = { 0 };
-    while (!feof(file)) {
-        file_get_line(buffer, 512, file);
-        lines++;
-    }
-#else
-    s32 lines = 1;
-    char c;
-    do {
-        c = fgetc(file);
-        if (c == '\n') { lines++; }
-    } while (c != EOF);
-#endif
-
-    fclose(file);
-    
-    return lines - 4;
-}
-
-void mod_storage_get_filename(char* dest) {
+void Mod_Storage_Get_Filename(char* dest) {
     const char *path = sys_user_path(); // get base sm64ex-coop appdata dir
     snprintf(dest, SYS_MAX_PATH - 1, "%s/sav/%s", path, gLuaActiveMod->relativePath); // append sav folder
-    strdelete(dest, ".lua"); // delete ".lua" from sav name
+    strdelete(dest, (char *)".lua"); // delete ".lua" from sav name
     strcat(dest, SAVE_EXTENSION); // append SAVE_EXTENSION
     normalize_path(dest); // fix any out of place slashes
 }
 
+extern "C" {
+
 bool mod_storage_save(const char *key, const char *value) {
     if (strlen(key) > MAX_KEY_VALUE_LENGTH || strlen(value) > MAX_KEY_VALUE_LENGTH) {
-        LOG_LUA_LINE("Too long of a key and or value for mod_storage_save()");
         return false;
     }
-    if (!char_valid((char *)key) || !char_valid((char *)value)) {
-        LOG_LUA_LINE("Invalid key and or value passed to mod_storage_save()");
+    if (!Char_Valid((char *)key) || !Char_Valid((char *)value)) {
         return false;
     }
 
@@ -144,86 +122,49 @@ bool mod_storage_save(const char *key, const char *value) {
     }
 #endif
 
-    FILE *file;
-    Config *cfg = NULL;
-    char *filename;
-    filename = (char *)malloc((SYS_MAX_PATH - 1) * sizeof(char));
-    mod_storage_get_filename(filename);
+    char filename[SYS_MAX_PATH] = {0};
+    Mod_Storage_Get_Filename(filename);
 
     // ensure savPath exists
     char savPath[SYS_MAX_PATH] = { 0 };
     if (snprintf(savPath, SYS_MAX_PATH - 1, "%s", fs_get_write_path(SAVE_DIRECTORY)) < 0) {
-        LOG_ERROR("Failed to concat sav path");
-        free(filename);
         return false;
     }
     if (!fs_sys_dir_exists(savPath)) { fs_sys_mkdir(savPath); }
 
-    bool exists = fs_sys_path_exists(filename);
-    file = fopen(filename, exists ? "r+" : "w");
-    cfg = ConfigNew();
-    if (exists) {
-        if (ConfigReadFile(filename, &cfg) != CONFIG_OK) {
-            ConfigFree(cfg);
-            fclose(file);
-            free(filename);
-            return false;
-        }
-        if (key_count(filename) > MAX_KEYS) {
-            LOG_LUA_LINE("Tried to save more than MAX_KEYS with mod_storage_save()");
-            ConfigFree(cfg);
-            fclose(file);
-            free(filename);
-            return false;
-        }
+    mINI::INIFile file(filename);
+    mINI::INIStructure ini;
+    file.read(ini);
+
+    if (ini["storage"].size() + 1 > MAX_KEYS) {
+        return false;
     }
 
-    char lowerKey[MAX_KEY_VALUE_LENGTH];
-    snprintf(lowerKey, MAX_KEY_VALUE_LENGTH, "%s", key);
-    for (int i = 0; i < MAX_KEY_VALUE_LENGTH; i++) {
-        if (lowerKey[i] == '\0') { break; }
-        lowerKey[i] = tolower(lowerKey[i]);
-    }
+    ini["storage"][key] = value;
 
-    ConfigRemoveKey(cfg, "storage", lowerKey);
-    ConfigRemoveKey(cfg, "storage", key);
-    ConfigAddString(cfg, "storage", key, value);
+    file.write(ini);
 
-    fclose(file);
-    ConfigPrintToFile(cfg, filename);
-    ConfigFree(cfg);
-    free(filename);
+    file.generate(ini);
 
     return true;
 }
 
-bool mod_storage_save_number(const char* key, f32 value) {
-    // Store string results in a temporary buffer
-    // this assumes mod_storage_load will only ever be called by Lua
-    static char str[MAX_KEY_VALUE_LENGTH];
-    if (floor(value) == value) {
-        snprintf(str, MAX_KEY_VALUE_LENGTH, "%lld", (s64)value);
-    } else {
-        snprintf(str, MAX_KEY_VALUE_LENGTH, "%f", value);
-    }
-
-    return mod_storage_save(key, str);
+bool mod_storage_save_number(const char* key, double value) {
+    return Mod_Storage_Save(key, std::to_string(value).c_str());
 }
 
 bool mod_storage_save_bool(const char* key, bool value) {
-    return mod_storage_save(key, value ? "true" : "false");
+    return Mod_Storage_Save(key, value ? "true" : "false");
 }
 
 const char *mod_storage_load(const char *key) {
     if (strlen(key) > MAX_KEY_VALUE_LENGTH) {
-        LOG_LUA_LINE("Too long of a key for mod_storage_load()");
         return NULL;
     }
-    if (!char_valid((char *)key)) {
-        LOG_LUA_LINE("Invalid key passed to mod_storage_save()");
+    if (!Char_Valid((char *)key)) {
         return NULL;
     }
-
+    
 #ifdef __ANDROID__
     char *cached_value = NULL;
     cached_value = key_cached(key, NULL);
@@ -232,47 +173,83 @@ const char *mod_storage_load(const char *key) {
     }
 #endif
 
-    char *filename;
-    filename = (char *)malloc((SYS_MAX_PATH - 1) * sizeof(char));
-    mod_storage_get_filename(filename);
-    static char value[MAX_KEY_VALUE_LENGTH];
-    ini_t *storage;
+    char filename[SYS_MAX_PATH] = {0};
+    Mod_Storage_Get_Filename(filename);
 
-    if (!fs_sys_path_exists(filename)) {
-        free(filename);
+    if (!path_exists(filename)) {
         return NULL;
     }
 
-    storage = ini_load(filename);
-    if (storage == NULL) {
-        ini_free(storage);
-        free(filename);
-        return NULL;
-    }
-    snprintf(value, MAX_KEY_VALUE_LENGTH, "%s", ini_get(storage, "storage", key));
+    mINI::INIFile file(filename);
+    mINI::INIStructure ini;
+    file.read(ini);
 
-    ini_free(storage);
-    free(filename);
-
-    if (strstr(value, "(null)") != NULL) { return NULL; }
+    return const_cast<char*>(ini["storage"][key].c_str());
 
 #ifdef __ANDROID__
     cache_key(key, value);
 #endif
 
-    return value;
 }
 
-f32 mod_storage_load_number(const char* key) {
-    const char* value = mod_storage_load(key);
+double mod_storage_load_number(const char *key) {
+    const char *value = Mod_Storage_Load(key);
     if (value == NULL) { return 0; }
 
-    return strtof(value, NULL);
+    return std::strtod(value, nullptr);
 }
 
-bool mod_storage_load_bool(const char* key) {
-    const char* value = mod_storage_load(key);
+bool mod_storage_load_bool(const char *key) {
+    const char *value = Mod_Storage_Load(key);
     if (value == NULL) { return false; }
 
     return !strcmp(value, "true");
+}
+
+bool mod_storage_remove(const char* key) {
+    if (gLuaActiveMod == NULL) { return false; }
+    if (strlen(key) > MAX_KEY_VALUE_LENGTH) { return false; }
+    if (!char_valid((char *)key)) { return false; }
+
+    char filename[SYS_MAX_PATH] = { 0 };
+    Mod_Storage_Get_Filename(filename);
+    if (!fs_sys_path_exists(filename)) { return false; }
+
+    mINI::INIFile file(filename);
+    mINI::INIStructure ini;
+    file.read(ini);
+
+    if (ini["storage"].remove(key)) {
+        file.write(ini);
+        file.generate(ini);
+        return true;
+    }
+
+    return false;
+}
+
+bool mod_storage_clear(void) {
+    char filename[SYS_MAX_PATH] = {0};
+    Mod_Storage_Get_Filename(filename);
+
+    if (!path_exists(filename)) {
+        return false;
+    }
+
+    mINI::INIFile file(filename);
+    mINI::INIStructure ini;
+    file.read(ini);
+
+    if (ini["storage"].size() == 0) {
+        return false;
+    }
+
+    ini["storage"].clear();
+
+    file.write(ini);
+
+    file.generate(ini);
+
+    return true;
+}
 }
--- src/pc/mods/mod_storage.h
+++ src/pc/mods/mod_storage.h
@@ -1,22 +1,35 @@
 #ifndef MOD_STORAGE_H
 #define MOD_STORAGE_H
 
-#include "mod.h"
-#include <stdbool.h>
+#include <PR/ultratypes.h>
 
-#define MAX_KEYS 255
-#define MAX_KEY_VALUE_LENGTH 64
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_KEYS 512
+#define MAX_KEY_VALUE_LENGTH 512
 #define SAVE_DIRECTORY "sav"
 #define SAVE_EXTENSION ".sav"
 
-bool mod_storage_save(const char *key, const char *value);
+bool mod_storage_save(const char* key, const char* value);
 bool mod_storage_save_number(const char* key, f32 value);
 bool mod_storage_save_bool(const char* key, bool value);
+
+const char *mod_storage_load(const char* key);
 f32 mod_storage_load_number(const char* key);
 bool mod_storage_load_bool(const char* key);
-const char *mod_storage_load(const char *key);
+
+bool mod_storage_remove(const char* key);
+bool mod_storage_clear(void);
+
 #ifdef __ANDROID__
 void key_cache_init(void);
 #endif
 
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif // MOD_STORAGE_H
