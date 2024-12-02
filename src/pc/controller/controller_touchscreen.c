7a018a5099add448da15523559e98dc6e8d61fdb
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2024-12-02 23:34:14 +0300

    fabsf

--- src/pc/controller/controller_touchscreen.c
+++ src/pc/controller/controller_touchscreen.c
@@ -433,7 +433,7 @@ static void DrawSpriteTexJoyBase(s32 x, s32 y, int scaling) {
 }
 
 s32 normalize_vectors(s32 x, s32 y, float dX, float dY) {
-    float divisor = abs(dX) + abs(dY);
+    float divisor = fabsf(dX) + fabsf(dY);
     if (divisor != 0) {
         return 0;
     } else {
