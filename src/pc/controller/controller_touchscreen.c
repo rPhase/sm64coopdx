c3198b39cd997aff58b9ec061d1d6015d44ceca1
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2024-12-08 19:00:19 +0300

    ";" damn

--- src/pc/controller/controller_touchscreen.c
+++ src/pc/controller/controller_touchscreen.c
@@ -442,8 +442,8 @@ static void DrawSpriteTexJoyBase(s32 x, s32 y, int scaling) {
 
 void render_button(Vtx *vtx, const u8 *texture, u32 fmt, u32 siz, s32 texW, s32 texH, s32 x, s32 y, s32 w, s32 h, s32 tileX, s32 tileY, s32 tileW, s32 tileH) {
 
-    s32 adjustedY = HUD_TOP_Y + y
-    s32 adjustedX = GFX_DIMENSIONS_RECT_FROM_LEFT_EDGE(x)
+    s32 adjustedY = HUD_TOP_Y + y;
+    s32 adjustedX = GFX_DIMENSIONS_RECT_FROM_LEFT_EDGE(x);
     create_dl_ortho_matrix();
     if (!vtx) {
         vtx = alloc_display_list(sizeof(Vtx) * 4);
