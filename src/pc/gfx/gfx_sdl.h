8634178b370e0d40a8903b683b4942c3b798dcbb
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-02-20 16:43:01 +0330

    try two

--- src/pc/gfx/gfx_sdl.h
+++ src/pc/gfx/gfx_sdl.h
@@ -5,4 +5,8 @@
 
 extern struct GfxWindowManagerAPI gfx_sdl;
 
+#ifdef TOUCH_CONTROLS
+void gfx_sdl_set_touchscreen_callbacks(void (*down)(void* event), void (*motion)(void* event), void (*up)(void* event));
+
+#endif
 #endif
