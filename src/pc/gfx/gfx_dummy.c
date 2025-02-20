a8a921573841a892d46718a14b41465dae331a25
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-02-20 16:41:26 +0330

    fix compilation in android

--- src/pc/gfx/gfx_dummy.c
+++ src/pc/gfx/gfx_dummy.c
@@ -201,6 +201,9 @@ static void gfx_dummy_renderer_shutdown(void) {
 struct GfxWindowManagerAPI gfx_dummy_wm_api = {
     gfx_dummy_wm_init,
     gfx_dummy_wm_set_keyboard_callbacks,
+#ifdef TOUCH_CONTROLS // fix compilation in android
+    gfx_sdl_set_touchscreen_callbacks,
+#endif
     gfx_dummy_wm_main_loop,
     gfx_dummy_wm_get_dimensions,
     gfx_dummy_wm_handle_events,
