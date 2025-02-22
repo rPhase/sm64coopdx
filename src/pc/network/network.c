b9cb7b7f05371127f99e8276c62a33e5d8430ab5
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-02-22 15:24:09 +0330

    bump manifest xml version

--- src/pc/network/network.c
+++ src/pc/network/network.c
@@ -117,9 +117,9 @@ bool network_init(enum NetworkType inNetworkType, bool reconnecting) {
     }
 
     network_forget_all_reliable();
-    #ifndef TARGET_ANDROID
+#ifndef TARGET_ANDROID
     crash_handler_init();
-    #endif
+#endif
 
     // set server settings
     gServerSettings.playerInteractions = configPlayerInteraction;
