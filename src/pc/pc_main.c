fd419a864ff8529aad4c5d06318e7a44aba880d7
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-03-25 10:46:00 +0330

    [build] show_update_popup uncomment

--- src/pc/pc_main.c
+++ src/pc/pc_main.c
@@ -393,9 +393,7 @@ void* main_game_init(UNUSED void* dummy) {
     sync_objects_init_system();
 
     if (gCLIOpts.network != NT_SERVER && !gCLIOpts.skipUpdateCheck) {
-#ifndef __ANDROID__
         check_for_updates();
-#endif
     }
 
     LOADING_SCREEN_MUTEX(loading_screen_set_segment_text("Loading ROM Assets"));
@@ -554,7 +552,7 @@ int main(int argc, char *argv[]) {
     djui_init_late();
     djui_console_message_dequeue();
 
-    //show_update_popup();
+    show_update_popup();
 
     // initialize network
     if (gCLIOpts.network == NT_CLIENT) {
