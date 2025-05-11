bfcca0ca4346b0f0214abab3ead853c324c5ce51
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-05-11 18:43:24 +0330

    seperate android lobby

--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -4,7 +4,11 @@
 #define SM64COOPDX_VERSION "v1.3.2"
 
 // internal version
+#ifdef __ANDROID__
+#define VERSION_TEXT "av"
+#else
 #define VERSION_TEXT "v"
+#endif
 #define VERSION_NUMBER 40
 #define MINOR_VERSION_NUMBER 2
 
