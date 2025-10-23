9968f523c6575c8bf0c3b9cdbb0993269fd5162c
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2025-10-23 15:55:41 +0300

    curl java [build]

--- platform/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ platform/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -280,11 +280,12 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     protected String[] getLibraries() {
         return new String[] {
             "SDL2",
+            "curl"
+            "main"
             // "SDL2_image",
             // "SDL2_mixer",
             // "SDL2_net",
             // "SDL2_ttf",
-            "main"
         };
     }
 
