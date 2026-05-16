rPhase/sm64coopdx

commit bb142d5769a2b24e8b3b2db0b5edb3c240992fbe

Add searchbox, add it to mod panel and dynos panel (#1129)

src/pc/djui/djui_panel_dynos.c
@@ -10,6 +10,10 @@
 #include "game/level_update.h"
 #include "pc/lua/smlua_hooks.h"
 
+static struct DjuiFlowLayout* sDynosLayout = NULL;
+static struct DjuiPaginated* sDynosPaginated = NULL;
+static struct DjuiInputbox* sSearchInputbox = NULL;
+
 void djui_panel_dynos_create(struct DjuiBase* caller);
 
 static void djui_panel_dynos_apply(struct DjuiBase* caller) {
@@ -37,27 +41,47 @@ static void djui_panel_dynos_destroy(UNUSED struct DjuiBase* caller) {
     gDjuiInPlayerMenu = false;
 }
 
+static void djui_panel_dynos_add_packs(struct DjuiBase* base) {
+    int packCount = dynos_pack_get_count();
+    for (int i = 0; i < packCount; i++) {
+        if (!dynos_pack_get_exists(i)) { continue; }
+        bool tmp = dynos_pack_get_enabled(i);
+        const char* pack = dynos_pack_get_name(i);
+        // filter results
+        if (sSearchInputbox != NULL &&
+            sSearchInputbox->buffer != NULL &&
+            !strcasestr(djui_text_get_uncolored_string(NULL, strlen(pack) + 1, pack), sSearchInputbox->buffer)
+        ) {
+            continue;
+        }
+
+        struct DjuiCheckbox* checkbox1 = djui_checkbox_create(base, pack, &tmp, djui_panel_dynos_apply);
+        checkbox1->base.tag = i;
+        checkbox1->base.bTag = tmp;
+        checkbox1->value = &checkbox1->base.bTag;
+    }
+}
+
+static void djui_panel_dynos_rebuild_list(UNUSED struct DjuiBase* caller) {
+    djui_base_destroy_children(&sDynosLayout->base);
+    djui_panel_dynos_add_packs(&sDynosLayout->base);
+    djui_paginated_calculate_height(sDynosPaginated);
+}
+
 void djui_panel_dynos_create(struct DjuiBase* caller) {
     gDjuiInPlayerMenu = true;
-    int packCount = dynos_pack_get_count();
     struct DjuiThreePanel* panel = djui_panel_menu_create(DLANG(DYNOS, DYNOS), true);
     struct DjuiBase* body = djui_three_panel_get_body(panel);
-
     {
+        struct DjuiSearchbox* searchbox = djui_searchbox_create(body, djui_panel_dynos_rebuild_list);
+        sSearchInputbox = searchbox->inputbox;
+
         struct DjuiPaginated* paginated = djui_paginated_create(body, 8);
         struct DjuiBase* layoutBase = &paginated->layout->base;
-        for (int i = 0; i < packCount; i++) {
-            if (dynos_pack_get_exists(i)) {
-                bool tmp = dynos_pack_get_enabled(i);
-                const char* pack = dynos_pack_get_name(i);
-
-                struct DjuiCheckbox* checkbox1 = djui_checkbox_create(layoutBase, pack, &tmp, djui_panel_dynos_apply);
-                checkbox1->base.tag = i;
-                checkbox1->base.bTag = tmp;
-                checkbox1->value = &checkbox1->base.bTag;
-            }
-        }
+        sDynosLayout = paginated->layout;
+        djui_panel_dynos_add_packs(layoutBase);
         djui_paginated_calculate_height(paginated);
+        sDynosPaginated = paginated;
 
         struct DjuiRect* space = djui_rect_create(body);
         djui_base_set_size_type(&space->base, DJUI_SVT_ABSOLUTE, DJUI_SVT_ABSOLUTE);

src/pc/djui/djui_panel_host_mods.c
@@ -25,6 +25,7 @@ static struct DjuiText* sTooltip = NULL;
 static struct DjuiPaginated* sModPaginated = NULL;
 static struct DjuiButton* sBackButton = NULL;
 static struct DjuiButton* sRefreshButton = NULL;
+static struct DjuiInputbox* sSearchInputbox = NULL;
 static unsigned int sSelectedCategory = MOD_CATEGORY_ALL;
 static bool sWarned = false;
 
@@ -165,6 +166,13 @@ void djui_panel_host_mods_add_mods(struct DjuiBase* layoutBase) {
                 break;
             }
         }
+        // filter results
+        if (sSearchInputbox != NULL &&
+            sSearchInputbox->buffer != NULL &&
+            !strcasestr(djui_text_get_uncolored_string(NULL, strlen(mod->name) + 1, mod->name), sSearchInputbox->buffer)
+        ) {
+            continue;
+        }
         struct DjuiCheckbox* checkbox = djui_checkbox_create(layoutBase, mod->name, &mod->enabled, djui_mod_checkbox_on_value_change);
         checkbox->base.tag = i;
         djui_base_set_enabled(&checkbox->base, mod->selectable);
@@ -180,7 +188,7 @@ void djui_panel_host_mods_add_mods(struct DjuiBase* layoutBase) {
     }
 }
 
-static void djui_panel_on_categories_change(UNUSED struct DjuiBase* caller) {
+static void djui_panel_rebuild_mods_list(UNUSED struct DjuiBase* caller) {
     if (gModRefreshThread.state == RUNNING) { return; }
     djui_base_destroy_children(&sModLayout->base);
     djui_panel_host_mods_add_mods(&sModLayout->base);
@@ -225,14 +233,15 @@ void djui_panel_host_mods_create(struct DjuiBase* caller) {
 
     struct DjuiBase* body = djui_three_panel_get_body(panel);
     {
-        // copy category choices from sCategories
-        char* categoryChoices[sizeof(sCategories)];
+        struct DjuiSearchbox* searchbox = djui_searchbox_create(body, djui_panel_rebuild_mods_list);
+        sSearchInputbox = searchbox->inputbox;
 
-        // loop thru all categories names, and add those to the categoryChoices string array
+        char* categoryChoices[sizeof(sCategories)];
         for (int i = 0; i < numCategories; i++) {
             categoryChoices[i] = djui_language_get("HOST_MOD_CATEGORIES", sCategories[i].langKey);
         }
-        djui_selectionbox_create(body, DLANG(HOST_MODS, CATEGORIES), categoryChoices, numCategories, &sSelectedCategory, djui_panel_on_categories_change);
+        djui_selectionbox_create(body, DLANG(HOST_MODS, CATEGORIES), categoryChoices, numCategories, &sSelectedCategory, djui_panel_rebuild_mods_list);
+
         struct DjuiPaginated* paginated = djui_paginated_create(body, 8);
         paginated->showMaxCount = true;
         sModLayout = paginated->layout;
@@ -250,7 +259,7 @@ void djui_panel_host_mods_create(struct DjuiBase* caller) {
             djui_button_create(body, DLANG(MENU, BACK), DJUI_BUTTON_STYLE_BACK, djui_panel_menu_back);
         }
 
-        panel->bodySize.value = paginated->base.height.value + 64 + 64;
+        panel->bodySize.value = paginated->base.height.value + 64 + 64 + 64;
     }
 
     panel->base.destroy = djui_panel_host_mods_destroy;

lang/Czech.ini
@@ -64,6 +64,7 @@ BACK = "Zpět"
 CANCEL = "Zrušit"
 NO = "Ne"
 YES = "Ano"
+SEARCH = "Hledat..."
 
 [CAMERA]
 CAMERA = "KAMERA"

lang/Dutch.ini
@@ -64,6 +64,7 @@ BACK = "Terug"
 CANCEL = "Stop"
 NO = "Nee"
 YES = "Ja"
+SEARCH = "Zoek..."
 
 [CAMERA]
 CAMERA = "CAMERA"

lang/English.ini
@@ -64,6 +64,7 @@ BACK = "Back"
 CANCEL = "Cancel"
 NO = "No"
 YES = "Yes"
+SEARCH = "Search..."
 
 [CAMERA]
 CAMERA = "CAMERA"

lang/French.ini
@@ -64,6 +64,7 @@ BACK = "Retour"
 CANCEL = "Annuler"
 NO = "Non"
 YES = "Oui"
+SEARCH = "Rechercher..."
 
 [CAMERA]
 CAMERA = "CAMÉRA"

lang/German.ini
@@ -64,6 +64,7 @@ BACK = "Zurück"
 CANCEL = "Abbrechen"
 NO = "Nein"
 YES = "Ja"
+SEARCH = "Suche..."
 
 [CAMERA]
 CAMERA = "KAMERA"

lang/Italian.ini
@@ -64,6 +64,7 @@ BACK = "Indietro"
 CANCEL = "Annulla"
 NO = "No"
 YES = "Si"
+SEARCH = "Cerca..."
 
 [CAMERA]
 CAMERA = "TELECAMERA"

commit d750fee1795b109f8a30fce1aa28a32a885427bb

Resolve issue with windows compilation due to use of `strcasestr` (#1246)
Fixes issue introduced by https://github.com/coop-deluxe/sm64coopdx/commit/bb142d5769a2b24e8b3b2db0b5edb3c240992fbe because someone didn't check the C standard to verify it was ok to use (it was me).

`strcasestr` is not apart of the C standard and is a GNU extension. Windows does not play well with this. The solution was to define a quite simple custom function partially based on the glibc implementation. Function should be reviewed.

src/pc/djui/djui_panel_dynos.c
@@ -50,7 +50,7 @@ static void djui_panel_dynos_add_packs(struct DjuiBase* base) {
         // filter results
         if (sSearchInputbox != NULL &&
             sSearchInputbox->buffer != NULL &&
-            !strcasestr(djui_text_get_uncolored_string(NULL, strlen(pack) + 1, pack), sSearchInputbox->buffer)
+            !strstr_lowercased(djui_text_get_uncolored_string(NULL, strlen(pack) + 1, pack), sSearchInputbox->buffer)
         ) {
             continue;
         }

src/pc/djui/djui_panel_host_mods.c
@@ -143,7 +143,7 @@ void djui_panel_host_mods_add_mods(struct DjuiBase* layoutBase) {
         if (category != NULL) {
             category = !strcmp(category, "cs") ? "character" : category;
         }
-        
+
         switch (sSelectedCategory) {
             case MOD_CATEGORY_ALL: { break; }
             case MOD_CATEGORY_MISC: {
@@ -169,7 +169,7 @@ void djui_panel_host_mods_add_mods(struct DjuiBase* layoutBase) {
         // filter results
         if (sSearchInputbox != NULL &&
             sSearchInputbox->buffer != NULL &&
-            !strcasestr(djui_text_get_uncolored_string(NULL, strlen(mod->name) + 1, mod->name), sSearchInputbox->buffer)
+            !strstr_lowercased(djui_text_get_uncolored_string(NULL, strlen(mod->name) + 1, mod->name), sSearchInputbox->buffer)
         ) {
             continue;
         }

src/pc/utils/misc.c
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <float.h>
+#include <ctype.h>
 
 #include "misc.h"
 
@@ -600,6 +601,31 @@ void str_seperator_concat(char *output_buffer, int buffer_size, char** strings, 
             buffer_index += seperator_length;
         }
     }
+}
+
+const char *strstr_lowercased(const char *haystack, const char *needle) {
+    // sanity check
+    if (!*needle) {
+        return haystack;
+    }
+
+    while (*haystack) {
+        const char *h = haystack;
+        const char *n = needle;
+
+        while (*h && *n && tolower((unsigned char)*h) == tolower((unsigned char)*n)) {
+            ++h;
+            ++n;
+        }
+
+        if (!*n) {
+            return haystack;
+        }
+
+        ++haystack;
+    }
+
+    return NULL;
 }
 
 static char *get_update_path(void) {

src/pc/utils/misc.h
@@ -37,6 +37,7 @@ void delta_interpolate_mtx(Mtx* out, Mtx* a, Mtx* b, f32 delta);
 void detect_and_skip_mtx_interpolation(Mtx** mtxPrev, Mtx** mtx);
 
 void str_seperator_concat(char *output_buffer, int buffer_size, char** strings, int num_strings, char* seperator);
+const char *strstr_lowercased(const char *haystack, const char *needle);
 
 bool can_update_game(void);
 void update_game(void);