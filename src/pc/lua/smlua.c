f39b7978b36d8b7d5f14b5e26a67a2cbe1123ebe
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2026-05-25 19:14:11 +1000

    add a new flag as a precaution
    to prevent people accidentally compiling unsafe code.

--- src/pc/lua/smlua.c
+++ src/pc/lua/smlua.c
@@ -320,7 +320,7 @@ void smlua_init(void) {
 
     // load libraries
     luaopen_base(L);
-#if defined(DEVELOPMENT)
+#if defined(THIS_IS_VERY_UNSAFE_AND_I_KNOW_WHAT_IM_DOING)
     luaL_requiref(L, "debug", luaopen_debug, 1);
     luaL_requiref(L, "io", luaopen_io, 1);
     luaL_requiref(L, "os", luaopen_os, 1);
